b3b52329e02ce922db92c60486e0ff3997141e82
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-03-13 11:16:42 +0100

    first commit

---
+++ Checkpoint 4.py
@@ -0,0 +1,43 @@
+# Exercise 1
+
+list = ["Rubens", "Ballester", "Lillo", "Laura", "Fernandez"]
+tuple = ("Juan", "Javier", "Jose", "Josefa", "Pedro")
+float = 3.30
+int = 144
+decimal = 3.458999
+dictionary = {"name": "Rubens", "age": "18", "city": "Bilbao"}
+
+# Exercise 2
+import math
+
+rounded_float = math.ceil(float)
+
+# Exercise 3
+
+square_root = math.sqrt(int)
+
+# Exercise 4
+
+dictionary_first = dictionary["name"]
+
+# Exercise 5
+
+second_tuple = tuple[1]
+
+# Exercise 6
+
+list.append("Lafuente")
+
+# Exercise 7
+
+list[0] = "Manuel"
+
+# Exercise 8
+
+list.sort()
+
+# Exercise 9
+
+tuple += ("Isidoro",)
+
+
